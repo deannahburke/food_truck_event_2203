301bd25e2d7193cf2f9fa5a2e9b37d6f3b96486a
Deannah Burke <deannah@bierstadtlager.com>
2022-04-26 12:01:34 -0600

    Final

--- lib/event.rb
+++ lib/event.rb
@@ -20,5 +20,29 @@ class Event
       same_foods
   end
 
+  def sorted_item_list
+    array = @food_trucks.map { |truck| truck.inventory.keys }.flatten.uniq
+    array.sort_by { |item| item.name }
+  end
+
+  def overstocked_items
+    # require "pry";binding.pry
+  end
+
+  def total_inventory
+    total_inventory = {}
+    @food_trucks.each do |truck|
+      truck.inventory.each do |item, quantity|
+        if total_inventory[item].nil?
+            total_inventory[item] = {quantity: quantity, food_trucks: [truck]}
+        else
+          total_inventory[item][:quantity] += quantity
+          total_inventory[item][:food_trucks] << truck
+        end
+      end
+    end
+    total_inventory
+  end
+
 
 end
--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -62,6 +62,5 @@ RSpec.describe FoodTruck do
     expect(food_truck2.potential_revenue).to eq(345.00)
     expect(food_truck3.potential_revenue).to eq(243.75)
   end
-
-
+  
 end
